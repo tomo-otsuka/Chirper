a807b4d8e95da6e03182878b653338a1cb89a044
Tomo Otsuka <totsuka@hearsaycorp.com>
2016-08-15 19:59:02 -0700

    Use RecyclerView and ButterKnife

--- app/src/main/java/com/codepath/apps/Chirper/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/Chirper/activities/TimelineActivity.java
@@ -3,7 +3,7 @@ package com.codepath.apps.Chirper.activities;
 import com.codepath.apps.Chirper.R;
 import com.codepath.apps.Chirper.TwitterApplication;
 import com.codepath.apps.Chirper.TwitterClient;
-import com.codepath.apps.Chirper.adapters.TweetsArrayAdapter;
+import com.codepath.apps.Chirper.adapters.TweetsAdapter;
 import com.codepath.apps.Chirper.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -12,30 +12,33 @@ import org.json.JSONObject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
 
     private TwitterClient client;
     private ArrayList<Tweet> tweets;
-    private TweetsArrayAdapter tweetsAdapter;
+    private TweetsAdapter tweetsAdapter;
 
-    private ListView lvTweets;
+    @BindView(R.id.rvTweets) RecyclerView rvTweets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-
-        lvTweets = (ListView) findViewById(R.id.lvTweets);
+        ButterKnife.bind(this);
         tweets = new ArrayList<>();
-        tweetsAdapter = new TweetsArrayAdapter(this, tweets);
-        lvTweets.setAdapter(tweetsAdapter);
+        tweetsAdapter = new TweetsAdapter(this, tweets);
+        rvTweets.setAdapter(tweetsAdapter);
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
 
         client = TwitterApplication.getRestClient();
         populateTimeline();
---
+++ app/src/main/java/com/codepath/apps/Chirper/adapters/TweetsAdapter.java
@@ -0,0 +1,64 @@
+package com.codepath.apps.Chirper.adapters;
+
+import com.codepath.apps.Chirper.R;
+import com.codepath.apps.Chirper.models.Tweet;
+import com.squareup.picasso.Picasso;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+    private ArrayList<Tweet> mTweets;
+    private Context mContext;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.tvUsername) TextView tvUsername;
+        @BindView(R.id.tvBody) TextView tvBody;
+        @BindView(R.id.ivProfileImage) ImageView ivProfileImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public TweetsAdapter(Context context, ArrayList<Tweet> tweets) {
+        mContext = context;
+        mTweets = tweets;
+    }
+
+    @Override
+    public TweetsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View tweetView = inflater.inflate(R.layout.item_tweet, parent, false);
+        ViewHolder viewHolder = new ViewHolder(tweetView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(TweetsAdapter.ViewHolder holder, int position) {
+        Tweet tweet = mTweets.get(position);
+
+        holder.tvUsername.setText(tweet.getUser().getName());
+        holder.tvBody.setText(tweet.getText());
+        holder.ivProfileImage.setImageResource(0);
+        Picasso.with(mContext).load(tweet.getUser().getProfileImageUrl())
+                .into(holder.ivProfileImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTweets.size();
+    }
+}
